03f036732f9bfb1ab0603e004569f1f3f43f60c0
termOS <olegmanko479@gmail.com>
2026-02-20 13:58:19 +0200

    add a new file cheque.txt and write a 2 classes shop and PivPau_pivo_na_zakaz and methods of classes

--- shop_abrakadabra/shop.h
+++ shop_abrakadabra/shop.h
@@ -1,1 +1,57 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class shop
+{
+protected:
+	int discounts;
+	int choise;
+public:
+	void buy();
+	void discount();
+	void printCheque();
+};
+
+class PivPau_pivo_na_zakaz : public shop
+{
+private:
+	int beer = 5;
+	int wiskey = 10;
+	int rom = 11;
+public:
+	void buy()
+	{
+		cout << "--- Catalog ---" << endl;
+		cout << "1. Beer -- " << beer << "$" << endl << "2. Wiskey -- " << wiskey << "$" << endl << "3. Rom -- " << rom << "$" << endl;
+		cout << "Enter what u need to buy: ";
+		cin >> choise;
+	}
+
+	void discount()
+	{
+		beer = beer - 2;
+	}
+	
+	void printCheque(ofstream& onf)
+	{
+		if (choise == 1)
+		{
+			onf << "--- Cheque ---" << endl;
+			onf << "Beer for " << beer << "$";
+		}
+
+		if (choise == 2)
+		{
+			onf << "--- Cheque ---" << endl;
+			onf << "Wiskey for " << wiskey << "$";
+		}
+		if (choise == 3)
+		{
+			onf << "--- Cheque ---" << endl;
+			onf << "Rom for " << rom << "$";
+		}
+	}
+};
